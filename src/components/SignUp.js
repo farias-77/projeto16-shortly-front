71eee38bdabf71da2bac7551be8dbc98d96722e7
farias-77 <gabrielfalves2003@gmail.com>
2022-08-05 23:23:21 -0300

    adiciona página de cadastro

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function SignUp(){
-
     const navigate = useNavigate();
     const [ userInfo, setUserInfo ] = useState({
         name: "",
